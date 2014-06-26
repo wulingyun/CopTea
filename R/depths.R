545bd24475401e6f40f675fbecc0f4ad7eade8be
Ling-Yun Wu <wulingyun@gmail.com>
2014-06-26 14:13:18 +0800

    Add option of parallel computation to neeat_depths

--- R/depths.R
+++ R/depths.R
@@ -8,6 +8,7 @@
 #' @param core.sets Logical matrix indicated the core genes associated with specific functions or pathways.
 #' @param net The adjacent matrix of network.
 #' @param max.depth Integer for the maximum depth that will be computed.
+#' @param n.cpu The number of CPUs/cores used in the parallel computation.
 #' 
 #' @return This function returns a matrix with the same dimensions as \code{core.sets}, where the element
 #' \code{[i, j]} represents the depth of node \code{i} under the condition (function or pathway) \code{j}.
@@ -15,18 +16,31 @@
 #' @seealso \code{\link{neeat}}
 #' 
 #' @export
-neeat_depths <- function(core.sets, net, max.depth)
+neeat_depths <- function(core.sets, net, max.depth, n.cpu = 1)
 {
   net.edges <- net_edges(net)
   fun <- function(i) get_depths(column(core.sets, i), net.edges, max.depth)
-  sapply(seq_len(dim(core.sets)[2]), fun)
+  if (n.cpu > 1) {
+    if (.Platform$OS.type == "windows")
+      cl <- makeCluster(n.cpu)
+    else
+      cl <- makeForkCluster(n.cpu)
+    depths <- parSapply(cl, seq_len(dim(core.sets)[2]), fun)
+    stopCluster(cl)
+  }
+  else {
+    depths <- sapply(seq_len(dim(core.sets)[2]), fun)
+  }
+  depths
 }
 
 
+# Compute the node depths for a given core set
 get_depths <- function(core.set, net.edges, max.depth)
   .Call(NE_GetDepths, net.edges$edges, net.edges$index, core.set, max.depth)
 
 
+# Compute the edge depths from given node depths
 edge_depth <- function(node.depth, net.edges, max.depth)
 {
   edges <- net.edges$edges
