8fade47115853f9c1df51d58c0bae326d15c1f22
Ling-Yun Wu <wulingyun@gmail.com>
2016-06-02 22:20:44 +0800

    Change package name to CopTea

---
+++ R/CopTea-package.R
@@ -0,0 +1,27 @@
+#' CopTea - Combinatorial optimization based enrichment analysis
+#' 
+#' This package provides several combinatorial optimization based enrichment analysis
+#' tools for biomedical research.
+#'
+#' This package implemented a collection of functional enrichment analysis methods:
+#' 
+#' \itemize{
+#'   \item \code{\link{cea}} Combinatorial enrichment analysis
+#'   \item \code{\link{netgen}} Network enhanced generative model for enrichment analysis
+#'   \item \code{\link{neeat}} Network enhanced enrichment analysis
+#'   \item \code{\link{noa}} Network ontology analysis
+#' }
+#' 
+#' 
+#' 
+#' @name CopTea-package
+#' @aliases CopTea-package CopTea
+#' @docType package
+#' @keywords package
+#' 
+#' @references Ling-Yun Wu. Combinatiral optimization based enrichment analysis tool. Manuscript,
+#' 2016.
+#' 
+#' @useDynLib CopTea, .registration = TRUE
+#' 
+NULL
